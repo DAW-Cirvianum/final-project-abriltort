05eb39b06586c38f3f71999e307be0ad7193fb15
Abril <abril.tort@cirvianum.cat>
2026-01-14 20:52:27 +0100

    fitxer idioma catala

---
+++ back-end/resources/views/lang/ca/vaildation.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'accepted' => 'El camp :attribute ha de ser acceptat.',
+    'active_url' => 'El camp :attribute no és una URL vàlida.',
+    'after' => 'El camp :attribute ha de ser una data posterior a :date.',
+    'alpha' => 'El camp :attribute només pot contenir lletres.',
+    'alpha_dash' => 'El camp :attribute només pot contenir lletres, números i guions.',
+    'alpha_num' => 'El camp :attribute només pot contenir lletres i números.',
+    'array' => 'El camp :attribute ha de ser un array.',
+    'before' => 'El camp :attribute ha de ser una data anterior a :date.',
+    'between' => [
+        'numeric' => 'El camp :attribute ha de ser entre :min i :max.',
+        'file' => 'El fitxer :attribute ha de tenir entre :min i :max KB.',
+        'string' => 'El camp :attribute ha de tenir entre :min i :max caràcters.',
+        'array' => 'El camp :attribute ha de tenir entre :min i :max elements.',
+    ],
+    'boolean' => 'El camp :attribute ha de ser cert o fals.',
+    'confirmed' => 'La confirmació de :attribute no coincideix.',
+    'date' => 'El camp :attribute no és una data vàlida.',
+    'email' => 'El camp :attribute ha de ser un correu electrònic vàlid.',
+    'exists' => 'El valor de :attribute no és vàlid.',
+    'image' => 'El fitxer :attribute ha de ser una imatge vàlida.',
+    'in' => 'El valor de :attribute no és vàlid.',
+    'integer' => 'El camp :attribute ha de ser un nombre enter.',
+    'max' => [
+        'numeric' => 'El camp :attribute no pot ser superior a :max.',
+        'file' => 'El fitxer :attribute no pot superar :max KB.',
+        'string' => 'El camp :attribute no pot tenir més de :max caràcters.',
+        'array' => 'El camp :attribute no pot tenir més de :max elements.',
+    ],
+    'min' => [
+        'numeric' => 'El camp :attribute ha de ser com a mínim :min.',
+        'file' => 'El fitxer :attribute ha de tenir com a mínim :min KB.',
+        'string' => 'El camp :attribute ha de tenir com a mínim :min caràcters.',
+        'array' => 'El camp :attribute ha de tenir com a mínim :min elements.',
+    ],
+    'required' => 'El camp :attribute és obligatori.',
+    'unique' => 'El valor de :attribute ja està en ús.',
+    'url' => 'El format del camp :attribute no és vàlid.',
+];
